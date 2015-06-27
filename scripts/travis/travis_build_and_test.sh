75d4515b32a4764049a9189f7a60023e25da285b
Tea <tea.desouza@gmail.com>
2015-06-27 11:44:56 +0800

    Separate io dependencies

--- scripts/travis/travis_build_and_test.sh
+++ scripts/travis/travis_build_and_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script called by Travis to do a CPU-only build of and test Caffe.
+# Script called by Travis to build and test Caffe.
 
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
@@ -7,7 +7,11 @@ MAKE="make --jobs=$NUM_THREADS --keep-going"
 if $WITH_CMAKE; then
   mkdir build
   cd build
-  cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON ..
+  if $WITH_IO; then
+    cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON -DUSE_HDF5=ON -DUSE_OPENCV=ON -DUSE_LMDB=ON -DUSE_LEVELDB=ON -DUSE_SNAPPY=ON ..
+  else
+    cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON -DUSE_HDF5=OFF -DUSE_OPENCV=OFF -DUSE_LMDB=OFF -DUSE_LEVELDB=OFF -DUSE_SNAPPY=OFF ..
+  fi
   $MAKE
   if ! $WITH_CUDA; then
     $MAKE runtest
@@ -18,6 +22,13 @@ if $WITH_CMAKE; then
 else
   if ! $WITH_CUDA; then
     export CPU_ONLY=1
+  fi
+  if $WITH_IO; then
+    export USE_LMDB=1
+    export USE_LEVELDB=1
+    export USE_OPENCV=1
+    export USE_SNAPPY=1
+    export USE_HDF5=1
   fi
   $MAKE all test pycaffe warn lint || true
   if ! $WITH_CUDA; then
